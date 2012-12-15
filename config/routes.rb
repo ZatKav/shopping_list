2fc2dab97416bc061dd0be02c45bae193b351156
zat <giacomokavanagh@hotmail.com>
2012-12-15 11:27:05 +0000

    User model finished

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,6 @@ ShoppingList::Application.routes.draw do
 
   get "static_pages/about"
 
-  resources :lists
-
-
-  resources :users
-
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
